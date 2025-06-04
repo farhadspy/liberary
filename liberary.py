e254585ab7094f10bea43e24f6d5b7d89d286341
farhad <farhadkh736@gmail.com>
2025-06-04 21:18:35 +0330

    Update liberary.py

--- liberary.py
+++ liberary.py
@@ -1,10 +1,10 @@
-class liberary:
+class Library:
     def __init__(self):
-        self.name = "centeral_liberary"
+        self.name = "central_library"
         self.section = ...
         self.name_employee = ...
         self.id_employee = ...
-        self.age_employe = ...
+        self.age_employee = ...
         self.books = []
  
 class Book:
@@ -17,16 +17,16 @@ class Book:
         self.new_book = 1
         self.in_stock = in_stock
           
-    def add(self):
+    def add(self ,library):
         while self.new_book == 1:
             self.new_book = 1
             name = input("pls enter name of book: ")
             author = input("pls enter Author of book: ")
             book_cover = input("pls enter book_cover: ")
-            year_of_production = input("pls enter Year_of_production of book: ")
+            year_of_production = int(input("pls enter Year_of_production of book: "))
             publications = input("pls enter publications of book: ")
-            in_stock = input("pls enter Inventory of book: ")
-            for n in Book:
+            in_stock = int(input("pls enter Inventory of book: "))
+            for n in library.books:
                 if n.name == name and n.author == author and n.book_cover == book_cover and \
                 n.year_of_production == year_of_production and n.publications == publications and n.in_stock == in_stock:
                     print("This book is available in the library.")
@@ -34,16 +34,16 @@ class Book:
             
             if self.new_book == 1:
                 n_book = Book(name ,author ,book_cover ,year_of_production ,publications ,in_stock)
-                Book.append(n_book)
+                library.books.append(n_book)
                 self.new_book = 0
             else:
                 self.new_book = 0
     
-    def search(self , name):
+    def search(self , name ,library):
         flag = 0
         while True:
-            for n in Book:
-                if n.name == name:
+            for n in library.books:
+                if n.name.lower() == name.lower():   # جستجوی بدون توجه به حروف بزرگ/کوچک
                     print("Book specifications:")
                     print("name: ", n.name ,"\nAuthor: " , n.author , "\nbook_cover: " , n.book_cover , "\nYear_of_production: " ,
                     n.year_of_production , "\npublications: " , n.publications)
@@ -54,65 +54,66 @@ class Book:
             else:
                 break
                     
-    def remove(self):
-        Book.remove(self)
+    def remove(self ,library):
+        library.books.remove(self)
      
-    def borrow(self ,name ,author ,book_cover ,year_of_production ,publications):
-        for n in Book:
+    def borrow(self ,library ,name ,author ,book_cover ,year_of_production ,publications):
+        for n in library.books:
             if n.name == name and n.author == author and n.book_cover == book_cover and \
             n.year_of_production == year_of_production and n.publications == publications:
                 n.in_stock -= 1
                 
           
-class Member(liberary):
+class Member:
     def __init__(self):
         ...
 
 class Menu():
-    def celected(chose):
+    def celected(chose ,library):
         while True:
             if chose == 1:
                 print("add Book selected:")
-                Book.add()
+                Book.add(library)
             elif chose == 2:
                 print("search book selected: ")
                 search = input("pls enter name of book: ")
-                Book.search(search)
+                Book.search(search ,library)
             elif chose == 3:
                 print("remove book selected: ")
                 name = input("pls enter name of book: ")
                 author = input("pls enter Author of book: ")
                 book_cover = input("pls enter book_cover: ")
-                year_of_production = input("pls enter Year_of_production of book: ")
+                year_of_production = int(input("pls enter Year_of_production of book: "))
                 publications = input("pls enter publications of book: ")
-                for p in Book:
+                for p in Library.books:
                     if p.name == name and p.author == author and p.book_cover == book_cover and \
                     p.year_of_production == year_of_production and p.publications == publications:
-                        p.remove()
+                        p.remove(library)
             elif chose == 4:
                 print("borrow book selected: ")
                 name = input("pls enter name of book: ")
                 author = input("pls enter Author of book: ")
                 book_cover = input("pls enter book_cover: ")
-                year_of_production = input("pls enter Year_of_production of book: ")
+                year_of_production = int(input("pls enter Year_of_production of book: "))
                 publications = input("pls enter publications of book: ")
-                Book.borrow(name ,author ,book_cover ,year_of_production ,publications)
+                Book.borrow(library ,name ,author ,book_cover ,year_of_production ,publications)
             elif chose == 5:
                 print("bye 👋")
                 exit(0)
             
             
     
-        
+main_book = []       
 if __name__ == "__main__":
-    print("Welcome to centeral_liberary")
+    library = Library()
+    print("Welcome to centeral_library")
     while True:
         print("1-menu_of_book \n2-member_info")
-        chose = input("please chose the number: ")
+        chose = int(input("please chose the number: "))
         if chose == 1:
             print("1-add Book \n2-search book \n3-remove book \n4-borrow book \n5-exit menu")
-            number = input("please chose your request: ")
-            Menu.celected(number)
+            number = int(input("please chose your request: "))
+            Menu.celected(number ,library)
         elif chose == 2:
             Member.info()
     print("Thank you to use apps. bye👋 ")
