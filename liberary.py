a9e09fc991c0d22abfe40fcebc5ee6823b25add1
farhad <farhadkh736@gmail.com>
2025-06-06 23:42:51 +0330

    Update liberary.py

--- liberary.py
+++ liberary.py
@@ -6,6 +6,10 @@ class Library:
         self.id_employee = ...
         self.age_employee = ...
         self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Added {book.name} to {self.name}\n")
  
 class Book:
     def __init__(self ,name ,author ,book_cover ,year_of_production ,publications ,in_stock):
@@ -14,32 +18,31 @@ class Book:
         self.book_cover = book_cover
         self.year_of_production = year_of_production
         self.publications = publications
-        self.new_book = 1
         self.in_stock = in_stock
-          
-    def add(self ,library):
-        while self.new_book == 1:
-            self.new_book = 1
-            name = input("pls enter name of book: ")
-            author = input("pls enter Author of book: ")
-            book_cover = input("pls enter book_cover: ")
+        
+    @staticmethod      
+    def add(library):
+        name = input("pls enter name of book: ")
+        author = input("pls enter Author of book: ")
+        book_cover = input("pls enter book_cover: ")
+        try:
             year_of_production = int(input("pls enter Year_of_production of book: "))
-            publications = input("pls enter publications of book: ")
             in_stock = int(input("pls enter Inventory of book: "))
-            for n in library.books:
-                if n.name == name and n.author == author and n.book_cover == book_cover and \
-                n.year_of_production == year_of_production and n.publications == publications and n.in_stock == in_stock:
-                    print("This book is available in the library.")
-                    self.new_book = 0
+        except ValueError:
+            print("Year and Inventory must be numbers! Try again.\n")
+            return
+        publications = input("pls enter publications of book: ")
+        for n in library.books:
+            if n.name == name and n.author == author and n.book_cover == book_cover and \
+            n.year_of_production == year_of_production and n.publications == publications and n.in_stock == in_stock:
+                print("This book is available in the library.")
+                return
             
-            if self.new_book == 1:
-                n_book = Book(name ,author ,book_cover ,year_of_production ,publications ,in_stock)
-                library.books.append(n_book)
-                self.new_book = 0
-            else:
-                self.new_book = 0
+        n_book = Book(name ,author ,book_cover ,year_of_production ,publications ,in_stock)
+        library.add_book(n_book)
     
-    def search(self , name ,library):
+    @staticmethod
+    def search( name ,library):
         flag = 0
         while True:
             for n in library.books:
@@ -56,17 +59,30 @@ class Book:
                     
     def remove(self ,library):
         library.books.remove(self)
-     
-    def borrow(self ,library ,name ,author ,book_cover ,year_of_production ,publications):
+        return
+    
+    @staticmethod 
+    def borrow(library ,name ,author ,book_cover ,year_of_production ,publications):
         for n in library.books:
             if n.name == name and n.author == author and n.book_cover == book_cover and \
             n.year_of_production == year_of_production and n.publications == publications:
-                n.in_stock -= 1
+                if n.in_stock > 0:
+                    n.in_stock -= 1
+                    print(f"Borrowed {n.name}. Remaining stock: {n.in_stock}\n")
+                    return
+                else:
+                    print("No copies available for borrowing.\n")
+                    return
                 
           
 class Member:
-    def __init__(self):
-        ...
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
+
+    def info(self):
+        print(f"Member: {self.name}, ID: {self.member_id}, Borrowed books: {len(self.borrowed_books)}\n")
 
 class Menu():
     def celected(chose ,library):
@@ -85,10 +101,11 @@ class Menu():
                 book_cover = input("pls enter book_cover: ")
                 year_of_production = int(input("pls enter Year_of_production of book: "))
                 publications = input("pls enter publications of book: ")
-                for p in Library.books:
+                for p in library.books:
                     if p.name == name and p.author == author and p.book_cover == book_cover and \
                     p.year_of_production == year_of_production and p.publications == publications:
                         p.remove(library)
+                        print(f"{p.name} removed from library.\n")
             elif chose == 4:
                 print("borrow book selected: ")
                 name = input("pls enter name of book: ")
@@ -109,12 +126,23 @@ if __name__ == "__main__":
     print("Welcome to centeral_library")
     while True:
         print("1-menu_of_book \n2-member_info")
-        chose = int(input("please chose the number: "))
+        try:
+            chose = int(input("please chose the number: "))
+        except ValueError:
+            print("Please enter a valid number!\n")
+            continue
         if chose == 1:
             print("1-add Book \n2-search book \n3-remove book \n4-borrow book \n5-exit menu")
-            number = int(input("please chose your request: "))
+            try:
+                number = int(input("please chose your request: "))
+            except ValueError:
+                print("Please enter a valid number!\n")
+                continue
             Menu.celected(number ,library)
         elif chose == 2:
-            Member.info()
+            member = print("Library members info: ")
+            name = input("pls enter name: ")
+            id = input("pls enter id: ")
+            member.info(name , id)
     print("Thank you to use apps. bye👋 ")
     
